230e82f113428c859931e5cd90428425bb08f440
Sonu Student <21cse56@vmkvec.edu.in>
2024-03-21 14:21:36 +0530

    completed 2hour 14minutes

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose =require("mongoose")
+const app=express()
+app.use(cors())
+const PORT=process.env.PORT||8081
+app.get("/",(req,res)=>{
+    res.send("server is runnig")
+})
+app.post("/signup",(req,res)=>{
+    console.log(req.body)
+})
+app.listen(PORT,()=>console.log("server is running at port:"+PORT))
